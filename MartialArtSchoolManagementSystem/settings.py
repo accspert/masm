4ce0f0e7ee490e0e8d06a48792604b2b35c079ef
accspert <wirkaufenelektronik@gmail.com>
2023-02-26 10:00:46 +0100

    settings

--- MartialArtSchoolManagementSystem/settings.py
+++ MartialArtSchoolManagementSystem/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'djangobower',
     'schedule'
 ]
 
@@ -140,7 +139,6 @@ STATICFILES_DIRS=[
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'djangobower.finders.BowerFinder',
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
