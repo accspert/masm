37388be69b3156f2810ae0188992e682b43d9f6e
Rumman Ahmar <72764336+rumman-ahmar@users.noreply.github.com>
2023-03-09 12:51:40 +0530

    complete code

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Student, Teacher, Course, CourseSchedule
+
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(CourseSchedule)
