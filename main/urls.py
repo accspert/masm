12ee4d80e5dedd61c6e4cfa168fc65fb07189894
Rumman Ahmar <72764336+rumman-ahmar@users.noreply.github.com>
2023-03-09 17:20:59 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,19 +1,25 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from .views import (
-    IndexView, StudentView,
+    IndexView, LoginView, LogOutView,
+    SignUpView, StudentView,
     TeacherView, UpdateRecordView,
     AddRecordView, DeleteRecordView
     )
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index"),
-    path('students/', StudentView.as_view(), name='students'),
-    path('teachers/', TeacherView.as_view(), name="teachers"),
-    path('add/<str:record_type>/', AddRecordView.as_view(), name="add_record"),
-    path('<str:record_type>/<int:id>', UpdateRecordView.as_view(),
-         name="update_records"),
-    path('add/<str:record_type>/<int:id>', DeleteRecordView.as_view(),
-         name="delete_record")
+    path('login/', LoginView.as_view(), name="login"),
+    path('signup/', SignUpView.as_view(), name="signup"),
+    path('logout/', login_required(LogOutView.as_view()), name="logout"),
+    path('', login_required(IndexView.as_view()), name="index"),
+    path('students/', login_required(StudentView.as_view()), name='students'),
+    path('teachers/', login_required(TeacherView.as_view()), name="teachers"),
+    path('add/<str:record_type>/', login_required(
+        AddRecordView.as_view()), name="add_record"),
+    path('<str:record_type>/<int:id>', login_required(
+        UpdateRecordView.as_view()), name="update_records"),
+    path('add/<str:record_type>/<int:id>', login_required(
+        DeleteRecordView.as_view()), name="delete_record")
 ]
